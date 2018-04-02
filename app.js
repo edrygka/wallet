357dde405e51063b930a23763063e77b2da64f35
edrygka <edrygha@gmail.com>
2018-04-02 20:36:13 +0300

    какой то зашквар, у меня горит

--- app.js
+++ app.js
@@ -1,36 +1,54 @@
 const chalk = require('chalk')
 const clear = require('clear')
 const figlet = require('figlet')
-const inquirer  = require('./lib/inquirer')
+const questions  = require('./lib/questions')
 
 const files = require('./lib/files')
 
 clear()
 console.log(
   chalk.yellow(
-    figlet.textSync('AXEL wallet', { horizontalLayout: 'full' })
+    figlet.textSync('My first wallet', { horizontalLayout: 'full' })
   )
 )
 files.getCurrentDirectoryBase()
 
 
-const run = async () => {
-  if (files.directoryExists('.wallet')) {
-    console.log(chalk.red('Wallet is finded!'))
-    const credentials = await inquirer.askCredentials()
-    //console.log(credentials)
-
-  } else {
-    console.log(chalk.red("Wallet is not exist, do you want create new one?"))
-    const result = await inquirer.askBool()
-    if(result){
-      files.createDirectory()
-    }
-  }
-
-  
+function error_output(message,loglevel)
+{
+    console.log("{ status: \"error\", message: \""+message+"\"}");
 }
 
-run()
+
+async function main() {
+  if (files.directoryExists('.wallet')) {
+    console.log(chalk.red('Wallet is finded!'))
+    const credentials = await questions.askCredentials()
+    //entering wallet 
+  } else {
+    console.log(chalk.red("Wallet is not exist, do you want create new one?"))
+    const result = await questions.askBool()
+    console.log(result)
+
+    if(result.answer == "Yes"){
+
+      const passphrase = await questions.askGithubCredential()
+
+      const opts = {
+        passPhrase: passphrase.password
+      }
+      files.createDirectory().then((res) => {
+        //TypeError: Cannot read property 'then' of undefined
+        console.log(data)
+        //need return path of directory where create json file
+        // create account 
+        //files.createJsonFile(path, "wallet.json", data, opts)
+      })
+    }
+  }
+} 
+
+
+main()
 
 
